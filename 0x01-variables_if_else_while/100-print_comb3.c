4195d505d2c319992468bda5086100b7420bb020
sergedja <pohiry.serge@gmail.com>
2023-07-18 09:40:53 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,42 +1,29 @@
-/*
- * File: 100-print_comb3.c
- * Auth: serge dja
- */
-
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: print all possible different
- * combinations of two digits.
- *
- * Return: Always 0 (Success)
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
-
 int main(void)
 {
-    int digit1 = 0;
+	int n, m;
 
-    while (digit1 <= 8)
-    {
-        int digit2 = digit1 + 1;
-
-        while (digit2 <= 9)
-        {
-            putchar(digit1 + '0');
-            putchar(digit2 + '0');
-
-            if (digit1 != 8 || digit2 != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            digit2++;
-        }
-        digit1++;
-    }
-
-    putchar('\n');
-    return (0);
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }	
