3ee97addd8747583d5bf7b87f0f07fdbd10066c5
sergedja <pohiry.serge@gmail.com>
2023-07-18 22:24:09 -0700

    Your commit message here

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 
 int main(void)
 {
-
-printf("_putchar\n");
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
