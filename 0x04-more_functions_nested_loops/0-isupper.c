f510542233bf3851510cec565df4f6fb852cc4dc
sergedja <pohiry.serge@gmail.com>
2023-07-22 16:09:59 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
+
+0-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
