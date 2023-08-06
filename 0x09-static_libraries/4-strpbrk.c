9a56075cc7d39c34594924602c57967a25a7797e
sergedja <pohiry.serge@gmail.com>
2023-08-06 15:44:24 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int k;
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
+
+============================
+
+5-strstr.c
+
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *l = haystack;
+char *p = needle;
+while (*l == *p && *p != '\0')
+{
+l++;
+p++;
+}
+if (*p == '\0')
+return (haystack);
+}
+return (0);
+}
